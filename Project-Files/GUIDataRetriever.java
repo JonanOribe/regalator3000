12139761dffdc084a5dc072e804c6fd66bcd5a62
ramonVD <rvicentediaz@gmail.com>
2017-03-15 22:35:52 +0100

    Clase para llenar la GUI con datos de la BBDD
    Por ahora un metodo general para obtener los nombres/tipo de marcas, categorias y regalos que listaremos en la GUI

---
+++ Project-Files/GUIDataRetriever.java
@@ -0,0 +1,55 @@
+package regalator3000;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class GUIDataRetriever {
+	
+	/*Puede devolver null, combinaciones aceptadas -> nombre + marcas/regalos; tipo + categorias*/
+	public static String[] getAllElements(DatabaseHandler DbConnector, String columna, String tabla){
+		 try {
+			 if (columna.equals("nombre")) {	
+				 if (!(tabla.equals("regalos") || tabla.equals("marcas"))) {  return null; 	 } //combinacion no permitida
+			 }
+			 else if (columna.equals("tipo")) {
+				 if (!tabla.equals("categorias")) {  return null; } //combinacion no permitida
+			 }
+			 else {
+				 System.out.println("Unknown column type");
+				 return null; //columna desconocida
+			 }
+	         Statement stmt = DbConnector.openNewConnection().createStatement(); 
+	         String codigoSQL = "SELECT "+columna+" FROM "+tabla+";";
+	         ResultSet rs = stmt.executeQuery(codigoSQL); //podemos reciclar stmts despues de usarlos?  pos se ve k si
+	         ArrayList<String> allValues = new ArrayList<String>();
+	         while(rs.next()){
+	        	 allValues.add(rs.getString(columna));
+	         }
+	         String[] valuesString = new String[allValues.size()]; //Pasamos la arrayList a String[] porque es lo que la GUI usaba
+	         for (int i = 0; i < allValues.size();i++){
+	        	 valuesString[i] = allValues.get(i);
+	         }
+	         rs.close();
+	         stmt.close();
+	         return valuesString;      
+		     }
+		     catch(Exception e){ //separar entre por duplicate key, por...
+		    	 System.out.println("a" + e.toString());
+		    	 return null;
+		     }
+			 finally{
+				 DbConnector.closeConnection();
+			 }
+	}
+
+	public static void main(String[] args) {
+		/*DatabaseHandler DbConnector = new DatabaseHandler();
+		String[] test = getAllElements(DbConnector, "nombre", "marcas");
+		if (test != null) {
+			System.out.println(Arrays.toString(test));
+		}*/
+	}
+
+}
